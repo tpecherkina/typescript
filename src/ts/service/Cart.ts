82bcf4e673d7ed32dc1c0a6f4d0114286ad3a884
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2022-03-27 15:25:53 -0400

    Update Cart.ts

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -9,5 +9,24 @@ export default class Cart {
 
     get items(): Buyable[] {
         return [...this._items]; 
+          }
+  
+  allSum() {
+        return this._items.reduce((total, item) => total + item.price, 0);
+    }
+
+    allSumDiscount(discountAmount: number): number {
+        return this.allSum() * discountAmount / 100;
+    }
+
+    finalTotalSum(discountValue: number): number {
+        return this.allSum() - this.allSumDiscount(discountValue);
+    }
+
+    deleteCart(id: number): void {
+        const index = this._items.findIndex((item) => item.id === id);
+        if (index !== -1) {
+            this._items.splice(index, 1);
+        }
     }
 }
